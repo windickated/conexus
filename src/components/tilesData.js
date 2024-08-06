29bc43c941ba6ee932934e9d55ab6548332a7632
Dimon <notawid@gmail.com>
2024-08-06 11:19:28 +0200

    Defined stories data array

---
+++ src/components/tilesData.js
@@ -0,0 +1,153 @@
+const stories = [
+  {
+    section: 'Collabs',
+    subsection: [
+      {
+        name: 'OUR COLLABORATIONS',
+        story: [
+          {
+            title: 'GLMR Apes',
+            description: `
+              Play as a hyper-intelligent Ape that's been captured by an AI Archon known as The Collector, and try to escape the Dark Zoo, surrounded by the Jailer, and other animals, threats, and allies.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/GLMRApes1.avif',
+            secondaryThumbnail: '/titlePicture/GLMRApes2.avif',
+            descriptionImage: '/descriptionPicture/GLMRApes.avif'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    section: 'Dischordian Saga',
+    subsection: [
+      {
+        name: 'GENERAL',
+        story: [
+          {
+            title: 'Escape',
+            description: `
+              Play as a prisoner going through experiments in a guarded prison and try to escape the planet by defeating or fleeing from The Warden.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/Escape1.avif',
+            secondaryThumbnail: '/titlePicture/Escape2.avif',
+            descriptionImage: '/descriptionPicture/Escape.avif'
+          },
+          {
+            title: 'Arena',
+            description: `
+              Try to escape the prison planet by winning in a series of challenges which may include the gladiator's arena filled with powerful creatures.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/Arena1.avif',
+            secondaryThumbnail: '/titlePicture/Arena2.avif',
+            descriptionImage: '/descriptionPicture/Arena.avif'
+          }
+        ]
+      },
+      {
+        name: 'CLASS GATED',
+        story: [
+          {
+            title: 'Assassin',
+            description: `
+              Play as Agent Zero, a highly trained assassin working for the forces of humanity, on a mission to infiltrate a base and assassinate an AI target vital to the war effort.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/Assassin1.avif',
+            secondaryThumbnail: '/titlePicture/Assassin2.avif',
+            descriptionImage: '/descriptionPicture/Assassin.avif'
+          },
+          {
+            title: 'Soldier',
+            description: `
+              Play as the Iron Lion, a legendary veteran fighting the AI empire.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/Soldier1.avif',
+            secondaryThumbnail: '/titlePicture/Soldier2.avif',
+            descriptionImage: '/descriptionPicture/Soldier.avif'
+          },
+          {
+            title: 'Spy',
+            description: `
+              Play as a double agent known as Eyes of the Watcher, choose between the Insurgency and the Architect's Watcher, and steal highly classified information from the other side.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/Spy1.avif',
+            secondaryThumbnail: '/titlePicture/Spy2.avif',
+            descriptionImage: '/descriptionPicture/Spy.avif'
+          },
+          {
+            title: 'Engineer',
+            description: `
+              Play as the Doctor, on a mission to fix the universe with inventive solutions, as you are visiting planets in danger from a variety of apocalyptic threats.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/Engineer1.avif',
+            secondaryThumbnail: '/titlePicture/Engineer2.avif',
+            descriptionImage: '/descriptionPicture/Engineer.avif'
+          },
+          {
+            title: 'Oracle',
+            description: `
+              Play as the Oracle, on a mission to spread the seeds of rebellion against the machine overlords and convince the people of a new world to join the Resistance.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/Oracle1.avif',
+            secondaryThumbnail: '/titlePicture/Oracle2.avif',
+            descriptionImage: '/descriptionPicture/Oracle.avif'
+          },
+          {
+            title: 'Ne-Yon',
+            description: `
+              Be a part of the rise of the Ne-Yons, a mysterious race of intergalactic demi-gods, and play a key part in the war between the Architect and humanity by deciding your allies, enemies, and using your higher powers.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/NeYon1.avif',
+            secondaryThumbnail: '/titlePicture/NeYon2.avif',
+            descriptionImage: '/descriptionPicture/NeYon.avif'
+          }
+        ]
+      },
+      {
+        name: 'TRAIT GATED',
+        story: [
+          {
+            title: 'Inception Ark',
+            description: `
+              Play as a prisoner going through experiments in a guarded prison and try to escape the planet by defeating or fleeing from The Warden.
+            `,
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/InceptionArk1.avif',
+            secondaryThumbnail: '/titlePicture/InceptionArk2.avif',
+            descriptionImage: '/descriptionPicture/InceptionArk.avif'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    section: 'Community Picks',
+    subsection: [
+      {
+        name: 'COMMUNITY PICKED STORIES',
+        story: [
+          {
+            title: 'Upcoming',
+            description: '',
+            storyLink: '',
+            primaryThumbnail: '/titlePicture/Upcoming.avif',
+            secondaryThumbnail: '/titlePicture/Upcoming.avif',
+            descriptionImage: '/titlePicture/Upcoming.avif'
+          }
+        ]
+      }
+    ]
+  }
+]
+
+
+export default stories;
