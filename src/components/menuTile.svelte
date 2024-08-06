30caa0be8d66f4974e99e9455a6662653438a7c0
Dimon <notawid@gmail.com>
2024-08-06 09:55:20 +0200

    Added menu tiles

---
+++ src/components/menuTile.svelte
@@ -0,0 +1,95 @@
+<script>
+  export let sectionName;
+  let sectionLink = sectionName.replace(/\s+/g, '');
+
+  const primaryThumbnail = `/titlePicture/${sectionLink}1.avif`;
+  const secondaryThumbnail = `/titlePicture/${sectionLink}2.avif`;
+
+  let isPrimary = true;
+  function tileHover() {
+    isPrimary = !isPrimary;
+  }
+</script>
+
+
+<a class="tile" id="{sectionName}" href="/{sectionLink}"
+  on:mouseenter={tileHover}
+  on:mouseleave={tileHover}
+  on:touchstart={tileHover}
+  on:touchend={tileHover}
+>
+  <img class="tile-picture {isPrimary ? 'visible' : ''}" src={primaryThumbnail} alt="{sectionName}" />
+  <img class="tile-picture {!isPrimary ? 'visible' : ''}" src={secondaryThumbnail} alt="{sectionName}" />
+  <p class="title">{ sectionName }</p>
+</a>
+
+
+<style>
+  .tile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 28vw;
+    height: 32vw;
+    margin: 1vw;
+    padding-bottom: 3%;
+    background-color: rgba(22, 30, 95, 0.75);
+    color: rgba(51, 226, 230, 0.75);
+    border: 0.05vw solid rgba(51, 226, 230, 0.75);
+    border-radius: 1.5vw;
+    filter: drop-shadow(0 0 0.1vw #010020);
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  .tile:hover, .tile:active {
+    background-color: rgba(51, 226, 230, 0.3);
+    color: #010020;
+    filter: drop-shadow(0 0 0.5vw #33e2e6);
+  }
+
+  .tile-picture {
+    display: none;
+    object-fit: cover;
+    width: 95%;
+    height: 80%;
+    margin: 2.5%;
+    margin-bottom: 0;
+    border: 0.05vw solid rgba(51, 226, 230, 0.75);
+    border-radius: 1vw;
+  }
+
+  .visible {
+    display: block;
+  }
+
+  .title {
+    font-size: 3vw;
+    line-height: 3vw;
+    white-space: nowrap;
+    text-shadow: 0 0 1vw #010020;
+  }
+
+
+  @media only screen and (max-width: 600px) {
+    .tile {
+      width: 80vw;
+      height: 60vw;
+      flex: none;
+      margin-bottom: 3vw;
+      padding-bottom: 0;
+      border-radius: 5vw;
+    }
+
+    .tile-picture {
+      width: 90%;
+      border-radius: 4vw;
+    }
+
+    .title {
+      font-size: 1.5em;
+      line-height: 1.5em;
+    }
+  }
+</style>
