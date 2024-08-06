cd7d82833075ed6d9df58d3ef8009e7ecbdf90aa
Dimon <notawid@gmail.com>
2024-08-06 13:28:04 +0200

    Added story tiles generation from array

---
+++ src/components/storyTile.svelte
@@ -0,0 +1,100 @@
+<script>
+  // import PopupTile from "./popupTile.svelte";
+
+  export let storyName;
+  export let storyDescription;
+  export let storyLink;
+  export let primaryThumbnail;
+  export let secondaryThumbnail;
+  export let descriptionPicture;
+
+  let isPrimary = true;
+  function tileHover() {
+    isPrimary = !isPrimary;
+  }
+
+  let showModal = false;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class="tile" id="{storyName}"
+  on:mouseenter={tileHover}
+  on:mouseleave={tileHover}
+  on:touchstart={tileHover}
+  on:touchend={tileHover}
+>
+  <img class="tile-picture {isPrimary ? 'visible' : ''}" src={primaryThumbnail} alt="{storyName}"/>
+  <img class="tile-picture {!isPrimary ? 'visible' : ''}" src={secondaryThumbnail} alt="{storyName}"/>
+  <p class="title">{ storyName }</p>
+</div>
+
+<!-- <PopupTile
+  {storyLink}
+  {storyDescription}
+  {descriptionPicture}
+  bind:showModal
+/> -->
+
+
+<style>
+  .tile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 20vw;
+    height: 22vw;
+    margin: 1vw;
+    padding-bottom: 3%;
+    background-color: rgba(36, 65, 189, 0.75);
+    color: rgba(255, 255, 255, 0.6);
+    border: 0.05vw solid rgba(51, 226, 230, 0.75);
+    border-radius: 1.5vw;
+    filter: drop-shadow(0 0 0.1vw #010020);
+    cursor: pointer;
+    text-decoration: none;
+    flex: none;
+  }
+
+  .tile:hover, .tile:active {
+    background-color: rgba(45, 90, 216, 0.9);
+    color: rgba(51, 226, 230, 0.9);
+    filter: drop-shadow(0 0 0.5vw rgba(51, 226, 230, 0.5));
+  }
+
+  .tile-picture {
+    display: none;
+    object-fit: cover;
+    width: 95%;
+    height: 80%;
+    margin: 2.5%;
+    margin-bottom: 0;
+    border: 0.05vw solid rgba(51, 226, 230, 0.75);
+    border-radius: 1vw;
+  }
+
+  .visible {
+    display: block;
+  }
+
+  .title {
+    font-size: 2.8vw;
+    line-height: 1vw;
+    white-space: nowrap;
+    text-shadow: 0 0 1vw rgba(1, 0, 32, 0.4);
+  }
+
+
+  @media only screen and (max-width: 600px) {
+    .tile {
+      width: 35vw;
+      height: 40vw;
+    }
+
+    .title {
+      font-size: 1.2em;
+      line-height: inherit;
+    }
+  }
+</style>
--- src/components/tilesData.js
+++ src/components/tilesData.js
@@ -3,7 +3,7 @@ const stories = [
     section: 'Collabs',
     subsection: [
       {
-        name: 'OUR COLLABORATIONS',
+        name: '',
         story: [
           {
             title: 'GLMR Apes',
@@ -11,9 +11,9 @@ const stories = [
               Play as a hyper-intelligent Ape that's been captured by an AI Archon known as The Collector, and try to escape the Dark Zoo, surrounded by the Jailer, and other animals, threats, and allies.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/GLMRApes1.avif',
-            secondaryThumbnail: '/titlePicture/GLMRApes2.avif',
-            descriptionImage: '/descriptionPicture/GLMRApes.avif'
+            primaryThumbnail: '/titlePicture/Collabs/GLMRApes1.avif',
+            secondaryThumbnail: '/titlePicture/Collabs/GLMRApes2.avif',
+            descriptionImage: '/descriptionPicture/Collabs/GLMRApes.avif'
           }
         ]
       }
@@ -31,9 +31,9 @@ const stories = [
               Play as a prisoner going through experiments in a guarded prison and try to escape the planet by defeating or fleeing from The Warden.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/Escape1.avif',
-            secondaryThumbnail: '/titlePicture/Escape2.avif',
-            descriptionImage: '/descriptionPicture/Escape.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/Escape1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/Escape2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/Escape.avif'
           },
           {
             title: 'Arena',
@@ -41,9 +41,9 @@ const stories = [
               Try to escape the prison planet by winning in a series of challenges which may include the gladiator's arena filled with powerful creatures.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/Arena1.avif',
-            secondaryThumbnail: '/titlePicture/Arena2.avif',
-            descriptionImage: '/descriptionPicture/Arena.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/Arena1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/Arena2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/Arena.avif'
           }
         ]
       },
@@ -56,9 +56,9 @@ const stories = [
               Play as Agent Zero, a highly trained assassin working for the forces of humanity, on a mission to infiltrate a base and assassinate an AI target vital to the war effort.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/Assassin1.avif',
-            secondaryThumbnail: '/titlePicture/Assassin2.avif',
-            descriptionImage: '/descriptionPicture/Assassin.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/Assassin1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/Assassin2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/Assassin.avif'
           },
           {
             title: 'Soldier',
@@ -66,9 +66,9 @@ const stories = [
               Play as the Iron Lion, a legendary veteran fighting the AI empire.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/Soldier1.avif',
-            secondaryThumbnail: '/titlePicture/Soldier2.avif',
-            descriptionImage: '/descriptionPicture/Soldier.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/Soldier1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/Soldier2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/Soldier.avif'
           },
           {
             title: 'Spy',
@@ -76,9 +76,9 @@ const stories = [
               Play as a double agent known as Eyes of the Watcher, choose between the Insurgency and the Architect's Watcher, and steal highly classified information from the other side.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/Spy1.avif',
-            secondaryThumbnail: '/titlePicture/Spy2.avif',
-            descriptionImage: '/descriptionPicture/Spy.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/Spy1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/Spy2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/Spy.avif'
           },
           {
             title: 'Engineer',
@@ -86,9 +86,9 @@ const stories = [
               Play as the Doctor, on a mission to fix the universe with inventive solutions, as you are visiting planets in danger from a variety of apocalyptic threats.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/Engineer1.avif',
-            secondaryThumbnail: '/titlePicture/Engineer2.avif',
-            descriptionImage: '/descriptionPicture/Engineer.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/Engineer1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/Engineer2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/Engineer.avif'
           },
           {
             title: 'Oracle',
@@ -96,9 +96,9 @@ const stories = [
               Play as the Oracle, on a mission to spread the seeds of rebellion against the machine overlords and convince the people of a new world to join the Resistance.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/Oracle1.avif',
-            secondaryThumbnail: '/titlePicture/Oracle2.avif',
-            descriptionImage: '/descriptionPicture/Oracle.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/Oracle1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/Oracle2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/Oracle.avif'
           },
           {
             title: 'Ne-Yon',
@@ -106,9 +106,9 @@ const stories = [
               Be a part of the rise of the Ne-Yons, a mysterious race of intergalactic demi-gods, and play a key part in the war between the Architect and humanity by deciding your allies, enemies, and using your higher powers.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/NeYon1.avif',
-            secondaryThumbnail: '/titlePicture/NeYon2.avif',
-            descriptionImage: '/descriptionPicture/NeYon.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/NeYon1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/NeYon2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/NeYon.avif'
           }
         ]
       },
@@ -121,9 +121,9 @@ const stories = [
               Play as a prisoner going through experiments in a guarded prison and try to escape the planet by defeating or fleeing from The Warden.
             `,
             storyLink: '',
-            primaryThumbnail: '/titlePicture/InceptionArk1.avif',
-            secondaryThumbnail: '/titlePicture/InceptionArk2.avif',
-            descriptionImage: '/descriptionPicture/InceptionArk.avif'
+            primaryThumbnail: '/titlePicture/DischordianSaga/InceptionArk1.avif',
+            secondaryThumbnail: '/titlePicture/DischordianSaga/InceptionArk2.avif',
+            descriptionImage: '/descriptionPicture/DischordianSaga/InceptionArk.avif'
           }
         ]
       }
@@ -133,15 +133,15 @@ const stories = [
     section: 'Community Picks',
     subsection: [
       {
-        name: 'COMMUNITY PICKED STORIES',
+        name: '',
         story: [
           {
             title: 'Upcoming',
             description: '',
             storyLink: '',
-            primaryThumbnail: '/titlePicture/Upcoming.avif',
-            secondaryThumbnail: '/titlePicture/Upcoming.avif',
-            descriptionImage: '/titlePicture/Upcoming.avif'
+            primaryThumbnail: '/titlePicture/CommunityPicks/Upcoming.avif',
+            secondaryThumbnail: '/titlePicture/CommunityPicks/Upcoming.avif',
+            descriptionImage: '/titlePicture/CommunityPicks/Upcoming.avif'
           }
         ]
       }
